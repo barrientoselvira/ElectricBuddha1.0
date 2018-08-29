5590fd2679d90226ae03b25868872bbf3a567fd9
spaceyb0t <elviracbarrientos@gmail.com>
2018-08-28 22:32:50 -0500

    component file structure

---
+++ client/components/Card/Card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  VrButton
+} from 'react-360';
--- client/index.js
+++ client/index.js
@@ -4,31 +4,36 @@ import {
   StyleSheet,
   Text,
   View,
+  VrButton
 } from 'react-360';
+
+import Navbar from './components/navbar';
 
 export default class client extends React.Component {
   render() {
     return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
-        </View>
-      </View>
+      <Navbar/>
+      // <View style={styles.panel}>
+      //   <View style={styles.greetingBox}>
+      //     <Text style={styles.greeting}>
+      //       Welcome to React 360
+      //     </Text>
+      //     <VrButton style={styles.button}/>
+      //   </View>
+      //   </View>
     );
   }
 };
 
 const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+  // panel: {
+  //   // Fill the entire surface
+  //   width: 1000,
+  //   height: 600,
+  //   backgroundColor: 'rgba(255, 255, 255, 0.4)',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // },
   greetingBox: {
     padding: 20,
     backgroundColor: '#000000',
@@ -37,6 +42,11 @@ const styles = StyleSheet.create({
   },
   greeting: {
     fontSize: 30,
+  },
+  button: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#ffffff'
   },
 });
 

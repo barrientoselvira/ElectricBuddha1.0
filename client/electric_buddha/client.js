72330dd896df6ea75cd20fcde114fb9802b0089b
danielt812 <danielt812@gmail.com>
2018-08-29 22:42:12 -0500

    New insights

--- client/electric_buddha/client.js
+++ client/electric_buddha/client.js
@@ -12,7 +12,7 @@ function init(bundle, parent, options = {}) {
 
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
-    r360.createRoot('client', { /* initial props */ }),
+    r360.createRoot('electric_buddha', { /* initial props */ }),
     r360.getDefaultSurface()
   );
 
